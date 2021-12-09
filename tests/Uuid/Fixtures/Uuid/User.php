734b0139b06cf5b508d66ed9c907f5881dc01c01
Maxim Smakouz <m.smakouz@gmail.com>
2021-12-09 18:46:39 +0200

    Added Ramsey UUID (#2)
    Co-authored-by: roxblnfk <roxblnfk@ya.ru>

---
+++ tests/Uuid/Fixtures/Uuid/User.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cycle\ORM\Entity\Macros\Uuid\Tests\Fixtures\Uuid;
+
+use Cycle\Annotated\Annotation\Column;
+use Cycle\Annotated\Annotation\Entity;
+use Cycle\ORM\Entity\Macros\Uuid\Uuid\Uuid1Macro;
+use Ramsey\Uuid\UuidInterface;
+
+#[Entity]
+#[Uuid1Macro]
+class User
+{
+    #[Column(type: 'uuid', primary: true)]
+    public UuidInterface $uuid;
+}
